6b49fb019f1d21b56e6bae048e08080e2c8c17fc
Sehrish Parvaiz Shah <155244290+Sehrishshah@users.noreply.github.com>
2026-05-21 23:08:32 +0530

    add SQL project

--- student management system.sql
+++ student management system.sql
@@ -6,10 +6,10 @@ create database students_database
  SELECT top 20 * from Student_Management_System
 
  --CHECKING FOR NULL AND DUPLICATE VALUES:
- SELECT COUNT(*) from Student_Management_System
+ SELECT COUNT(*) as NumberOfNulls from Student_Management_System
  where address IS NULL
 
- SELECT studentID,email,phone,COUNT(*) from Student_Management_System
+ SELECT studentID,email,phone,COUNT(*) as NumOfDup from Student_Management_System
  Group By studentID,email,phone
  HAVING COUNT(*)>1
 
@@ -28,6 +28,25 @@ ORDER BY count(studentID) DESC
 SELECT top 5 studentID, FirstName, CGPA from Student_Management_System
 ORDER BY CGPA desc
 
+
+ --Students below average CGPA:
+ SELECT studentID,
+       CONCAT(FirstName,' ',LastName) AS FullName,
+       CGPA
+FROM Student_Management_System
+WHERE CGPA <
+(
+SELECT AVG(CGPA)
+FROM Student_Management_System
+)
+
+
+--Highest and lowest CGPA in each department
+Select department,max(CGPA) as highest_CGPA,min(CGPA)  as lowest_CGPA 
+from Student_Management_System
+  GROUP BY Department
+
+
 --Creating a separate table to desplay only a few details of each student.
 CREATE VIEW student_details AS
 SELECT studentID,
@@ -44,15 +63,26 @@ SELECT * from student_details
  ORDER BY AVG(cgpa) desc
 
 
+
  --Student's enrolling year.
 SELECT studentID, concat(firstname,' ', lastname) as fullname,EnrollmentYear 
 from Student_Management_System
 
 
  -- Number of students enrolled each year
-SELECT Enrollmentyear , COUNT(StudentID) from Student_Management_System
-GROUP BY  EnrollmentYear
-ORDER BY enrollmentyear
+SELECT Enrollmentyear , COUNT(StudentID) as Number_Of_Enrollments_Per_Year 
+from Student_Management_System
+  GROUP BY  EnrollmentYear
+  ORDER BY Number_Of_Enrollments_Per_Year desc
+
+
+--Which year had the maximum enrollments?
+  SELECT TOP 1 EnrollmentYear,
+       COUNT(StudentID) AS TotalEnrollments
+FROM Student_Management_System
+GROUP BY EnrollmentYear
+ORDER BY TotalEnrollments DESC;
+
 
 
 /* 
@@ -69,9 +99,13 @@ ME dept has the highest CGPA.
 Architecture dept has a poor academic peformance
 
 3)Enrollment trends:
-Student enrollment analysis by year showed trends in admissions 
-and the rate of enrollment is decreasing over time.
+The analysis of 9000+ student records across 9 enrollment years (2015–2023) 
+showed that 2019 had the highest enrollment with 1,146 students, 
+indicating peak admission activity.
+Also Enrollment trends showed fluctuations over the years, with a 4.7% decline from the peak year (2019) to 2023,
+suggesting changing admission patterns.
 
 4)ME had 1720 students and the highest average CGPA was 3
 
 */
+
